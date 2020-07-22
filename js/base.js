f9bbadaaa7caf8896c6aba25bda45b7f599bfd2e
Callum Grimble <56933645+Grimbyy@users.noreply.github.com>
2020-07-22 21:50:11 +0100

    Update base.js

--- js/base.js
+++ js/base.js
@@ -7,7 +7,8 @@ async function initialText() {
 	sleep(5000);
 	$( "main" ).append( "<p>This is a test OS built with a single HTML document and a javascript file [using jquery]</p>" );
 	sleep(2000);
-	$( "main" ).append( "<p>What do you with to do? (Type help for commands)</p>" );
+	$( "main" ).append( "<p>What do you with to do? (Type help for commands): </p>" );
+	$( "p" )[$( "p" ).length - 1].append("<input type=""text"" placeholder=""Please type a command..."" style=""display: inline-block;"">");
 }
 
 demo();
