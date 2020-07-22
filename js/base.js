56eb0a95cf22750d056811322495203a1e949bda
Callum Grimble <56933645+Grimbyy@users.noreply.github.com>
2020-07-22 21:39:56 +0100

    Update base.js

--- js/base.js
+++ js/base.js
@@ -1,1 +1,9 @@
 console.log("IM WORKING!");
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+sleep(5000);
+$( "#welcome-msg" ).append( "<br><p>This is a test OS built with a single HTML document and a javascript file [using jquery]</p>" );
+sleep(2000);
+$( "p" )[-1].append( "<br><p>What do you with to do? (Type help for commands)</p>" );
